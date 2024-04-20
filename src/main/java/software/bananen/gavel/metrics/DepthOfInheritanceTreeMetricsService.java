71557b678165ce2aeb7ca7694b0f149615e5ded9
Dennis Reichenberg <9849919+SoylentBob@users.noreply.github.com>
2024-04-20 08:39:52 +0200

    Add comments to DepthOfInheritanceTreeMetricsService
    Detailed comments were added to the DepthOfInheritanceTreeMetricsService class and its measure method. The comments clearly explain the purpose of the class and the method, enhancing code readability and maintenance.

--- src/main/java/software/bananen/gavel/metrics/DepthOfInheritanceTreeMetricsService.java
+++ src/main/java/software/bananen/gavel/metrics/DepthOfInheritanceTreeMetricsService.java
@@ -6,8 +6,18 @@ import com.tngtech.archunit.core.domain.JavaClasses;
 import java.util.ArrayList;
 import java.util.Collection;
 
+/**
+ * A service that can be used to measure the depth of inheritance for the
+ * analyzed classes.
+ */
 public class DepthOfInheritanceTreeMetricsService {
 
+    /**
+     * Measures the metrics for the given classes.
+     *
+     * @param classes The classes that should be analyzed.
+     * @return The measurements.
+     */
     public Collection<DepthOfInheritanceTree> measure(final JavaClasses classes) {
         final Collection<DepthOfInheritanceTree> measurements =
                 new ArrayList<>();
