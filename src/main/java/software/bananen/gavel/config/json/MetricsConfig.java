3a3540fbc370100ca9dde455bbe80062cecd6175
Dennis Reichenberg <9849919+SoylentBob@users.noreply.github.com>
2024-10-24 20:15:39 +0200

    Remove unused code

--- src/main/java/software/bananen/gavel/config/json/MetricsConfig.java
+++
@@ -1,8 +0,0 @@
-package software.bananen.gavel.config.json;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public record MetricsConfig(
-        @JsonProperty("depth_of_inheritance") DepthOfInheritanceConfig depthOfInheritanceConfig,
-        @JsonProperty("change_coupling") ChangeCouplingConfig changeCouplingConfig) {
-}
